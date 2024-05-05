574e7a99706caf215cf677e4144aefab629a0dd0
dekisenpaitm <dekisenpaitm@gmail.com>
2024-05-05 20:45:22 +0200

    changed the parallax effect all over
    add spine to the project

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _playersRigidBody; 
     public float playerJumpForce; 
     public int playerJumpCount = 2;
-
+    private bool isMovementStopped = false;
 
     private void Start()
     {
@@ -24,18 +24,23 @@ public class PlayerController : MonoBehaviour
         //reading the values of the player movement direction for the players movement.
         _inputActionReference.Movement.Move.performed += moving =>
         {
-            _playersMovementDirection = moving.ReadValue<float>();
+
+                _playersMovementDirection = moving.ReadValue<float>();
+ 
         };
 
         _inputActionReference.Movement.Jump.performed += jumping => { JumpThePlayer(); };
     }
 
 
-    private void FixedUpdate()
+    private void Update()
     {
-        //Moving player using player rigid body.
-        _playersRigidBody.velocity =
-            new Vector2(_playersMovementDirection * playerSpeed, _playersRigidBody.velocity.y);
+        if (!isMovementStopped)
+        {
+            //Moving player using player rigid body.
+            _playersRigidBody.velocity =
+                    new Vector2(_playersMovementDirection * playerSpeed, _playersRigidBody.velocity.y);
+        }
     }
 
     private void JumpThePlayer()
@@ -48,15 +53,31 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log(collision.gameObject.tag);
         var contact = collision.GetContact(0);
         var contactPoint = contact.point;
         var ownCenter = contact.collider.bounds.center;
+        var ownBounds = contact.collider.bounds;
 
-        if (collision.gameObject.CompareTag("Floor") && contactPoint.y > ownCenter.y)
+        // Reset movement stop when hitting the floor
+        if (collision.gameObject.CompareTag("Floor"))
         {
-            playerJumpCount = 2;
+            // Check if the contact point is significantly above the center, indicating a non-leg hit
+            if (contactPoint.y > ownCenter.y + ownBounds.extents.y * 0.5)
+            {
+                Debug.LogError("HIT MY HEAD");
+                isMovementStopped = true;  // Stop movement if hit with an upper part of the bod
+            }
+            Debug.Log(isMovementStopped);
+            // Check if the contact point is at the top part of the collider (assuming legs are at the bottom)
+            if (contactPoint.y > ownCenter.y)
+            {
+                playerJumpCount = 2;  // Reset jump count if hit with legs
+                isMovementStopped = false;
+            }
         }
     }
 
